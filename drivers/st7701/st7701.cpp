8e20ab2ec1f1e28c2b2d2c05bc2b0754250880de
Mike Bell <mdb036@gmail.com>
2024-05-17 21:29:35 +0100

    Something sort of working

--- drivers/st7701/st7701.cpp
+++ drivers/st7701/st7701.cpp
@@ -223,7 +223,7 @@ void __no_inline_not_in_flash_func(ST7701::drive_timing)()
 
       // Test config - just repeat one word
       channel_config_set_ring(&config, false, 2);
-      static uint32_t test_colour = 0xFFFFFFFF;
+      static uint32_t test_colour = 0xFC00;
 
       //dma_channel_configure(st_dma, &config, &parallel_pio->txf[parallel_sm], NULL, 0, false);
       dma_channel_configure(st_dma, &config, &parallel_pio->txf[parallel_sm], &test_colour, 0xffffffff, true);
@@ -301,7 +301,7 @@ void __no_inline_not_in_flash_func(ST7701::drive_timing)()
     // Command 2 BK3
     command(reg::CND2BKxSEL, 5, "\x77\x01\x00\x00\x13");
     //command(reg::COLMOD, 1, "\x77");  // 24 bits per pixel...
-    command(reg::COLMOD, 1, "\x66");    // 18 bits per pixel...
+    command(reg::COLMOD, 1, "\x60");    // 18 bits per pixel...
     //command(reg::COLMOD, 1, "\x55");  // 16 bits per pixel...
 
     command(reg::SLPOUT);
